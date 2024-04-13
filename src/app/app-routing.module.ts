88325468fb7b423d212b19e797622fac9b8183da
polina <lolaschmidt@yandex.com>
2024-04-14 02:48:12 +0300

    initial of sign up page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { MainPageComponent } from './pages/main-page/main-page.component';
+import { SignInPageComponent } from './pages/sign-in-page/sign-in-page.component';
+import { SignUpPageComponent } from './pages/sign-up-page/sign-up-page.component';
+import { ProfilePageComponent } from './pages/profile-page/profile-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'main', component: MainPageComponent },
+  { path: 'sign-in', component: SignInPageComponent },
+  { path: 'sign-up', component: SignUpPageComponent },
+  { path: 'profile:id', component: ProfilePageComponent },
+  { path: '', redirectTo: '/main', pathMatch: 'full' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,31 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { AngularMaterialModule } from './angular-material.module';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SignInPageComponent } from './pages/sign-in-page/sign-in-page.component';
+import { SignUpPageComponent } from './pages/sign-up-page/sign-up-page.component';
+import { MainPageComponent } from './pages/main-page/main-page.component';
+import { ProfilePageComponent } from './pages/profile-page/profile-page.component';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SignInPageComponent,
+    SignUpPageComponent,
+    MainPageComponent,
+    ProfilePageComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AngularMaterialModule
   ],
-  providers: [],
+  providers: [
+    provideAnimationsAsync()
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
